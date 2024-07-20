f18d744181a649b3bf03ce9854be7ef434979795
anbananova <jiveumba99@yandex.ru>
2024-07-20 18:42:35 +0300

    fix: add @Repository annotation

--- stats/server/src/main/java/ru/yandex/practicum/repository/HitRepository.java
+++ stats/server/src/main/java/ru/yandex/practicum/repository/HitRepository.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.dto.StatsDto;
 import ru.yandex.practicum.model.Hit;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface HitRepository extends JpaRepository<Hit, Long> {
     @Query(value = "select app, uri, count(ip) as hits " +
             "from hits h " +
